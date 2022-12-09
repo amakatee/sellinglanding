66549379e8563d61dffa5b70bfcfd664675e587c
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-12-09 22:11:07 +0300

    revove

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import styles from '../styles/Home.module.css'
-// import { getBannerData } from '../lib/fetchApi'
-import axios from 'axios';
-const baseURL ='http://localhost:3000/api'
+import { getBannerData } from '../lib/fetchApi'
 
 
-export const getBannerData = async () => {
-  const res = await axios.get(`${baseURL}/banner`)
-  console.log(res.data[0])
-  return res.data[0]
-
-}
 export async function getStaticProps() {
   const bannerData = await getBannerData()
   return {
@@ -21,18 +13,18 @@ export async function getStaticProps() {
 export default function Home(props) {
   
 
-  const { data:bannerData } = useQuery({
+  const { data:bannerData, isLoading } = useQuery({
     queryKey: ['bannerData'],
     queryFn: getBannerData,
     initialData: props.bannerData,
   })
+  if(isLoading) return <div>Loading...</div>
 
   return (
     
     <div  >
 
-     ds{bannerData.secondText }
-     vds{bannerData.firstText }
+     ds{bannerData.secondText}
 
     </div>
   )
